5b8f8370377cefcedb0301655a0bf9a0624551c6
Kresten Laust <krestenlaust@gmail.com>
2023-12-16 13:49:10 +0100

    Basic population calculation

---
+++ calendar_populator.js
@@ -0,0 +1,19 @@
+function populate_calendar(table_element) {
+    const months = [
+        month_day_generator(8),
+        month_day_generator(9),
+        month_day_generator(10),
+        month_day_generator(11),
+        month_day_generator(0),
+    ]
+
+    // Generate days in table
+}
+
+/**
+ *
+ * @param month Zero-indexed month.
+ * @returns {Generator<*, void, *>}
+ */
+function *month_day_generator(month){
+}
